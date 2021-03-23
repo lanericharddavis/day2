899860877fdd32cecb9a1f9ca3209d5d24a7ed4d
Lane <lanericharddavis@gmail.com>
2021-03-23 10:29:37 -0600

    initial push

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+let cabinet = {
+  color: "white",
+  shape: "square",
+  size: [
+    {}
+    { width: 15 }
+  ],
+  hasKnobs: true,
+  numKnobs: 3,
+  contents: {
+    door1: ["coffee", "filters", "nonDairyCreamer"],
+    door2: ["forks", "spoons", "knives", "straws"],
+    door3: ["cups", "bowls", "plates"]
+  }
+}
+
+
