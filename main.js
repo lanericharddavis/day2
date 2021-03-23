5910a2cf07094474cc2523a829a9eb5c85e1a380
Lane <lanericharddavis@gmail.com>
2021-03-23 12:01:44 -0600

    added notes

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ console.log("Hello World")
 let cabinet = {
   color: "white",
   shape: "square",
-  size: [
-    {}
-    { width: 15 }
-  ],
   hasKnobs: true,
   numKnobs: 3,
   contents: {
@@ -16,4 +12,46 @@ let cabinet = {
   }
 }
 
+let welcomeMessage = "Today is a good day to Robot!"
 
+function greet(name, title) {
+  let greeting = `Hello ${title} ${name}! ${welcomeMessage}`
+  return greeting
+}
+
+let n = "Lane"
+let t = "Mr."
+
+let laneGreeting = greet(n, t)
+console.log(laneGreeting)
+
+
+let members = ['tim', 'jim', 'mary', 'margie', 'ned']
+
+let members2 = [
+  { name: "bob", title: "mr." },
+  { name: "hank", title: "mr." },
+  { name: "rob", title: "mr." },
+  { name: "tom", title: "mr." }
+]
+
+for (let i = 0; i < members.length; i++) {
+  const member = members[i];
+  console.log(greet(member))
+}
+
+for (let i = 0; i < members2.length; i++) {
+  const member = members2[i];
+  console.log(greet(member.name, member.title))
+}
+
+
+for (let i = 0; i < member2.length; i++) {
+  const member = members2[i]
+  console.log(greet(member.name, member.title))
+
+}
+
+members2.forEach(member => {
+  console.log(greet(member.name, member.title))
+});
